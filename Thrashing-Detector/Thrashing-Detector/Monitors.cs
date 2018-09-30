2d9ab526708b960b9c544e4f98a267993fdd441b
Brooks Olney <brooks@scaleit.net>
2018-09-29 21:17:44 -0400

    Preliminary Structure

---
+++ Thrashing-Detector/Thrashing-Detector/Monitors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Thrashing_Detector
+{
+    class Monitors
+    {
+        Processes _procs { get; set; }
+        Memory _memory { get; set; }
+        VirtualMemory _vm { get; set; }
+
+
+
+        internal Monitors()
+        {
+            // system resource objects
+            _procs = new Processes();
+            _memory = new Memory();
+            _vm = new VirtualMemory();
+        }
+
+        class Processes
+        {
+            List<Process> _procs = new List<Process>()
+                .Where(p => p.);
+
+            internal Processes()
+            {
+                _procs = Process.GetProcesses().ToList();
+            }
+        }
+
+        class Memory
+        {
+
+        }
+
+        class VirtualMemory
+        {
+
+        }
+
+
+        public void PollFunction()
+        {
+
+        }
+    }
+}
---
+++ Thrashing-Detector/Thrashing-Detector/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Thrashing_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing...");
+
+            //List<Process> _procs = new List<Process>(System.Diagnostics.Process.GetProcesses());
+            Monitors _monitors = new Monitors();
+            
+            Console.WriteLine("Testing...");
+        }
+    }
+}
