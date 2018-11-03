87c43a8fe295c058d70dab32c89ada244d602ccf
brooksolney <brooks@scaleit.net>
2018-11-02 20:31:02 -0400

    executable

--- Thrashing-Detector/Thrashing-Detector/Program.cs
+++ Thrashing-Detector/Thrashing-Detector/Program.cs
@@ -21,7 +21,7 @@ namespace Thrashing_Detector
                 _monitors.ThrashingCheck();
                 _monitors.processes();
                 ClearConsole();
-                Console.WriteLine("CPU Time: {0}%\nMemory Used: {1}%\nHard Page Faults/sec: {2}Thrashing counter: {3}",
+                Console.WriteLine("CPU Time: {0}%\nMemory Used: {1}%\nHard Page Faults/sec: {2}\nThrashing counter: {3}",
                     _monitors._procTime.ToString("N2"), _monitors._memoryUsed.ToString("N2"), _monitors._pageFaults, _monitors._thrashingCounter);
                 sw.WriteLine("{0}, {1}, {2}, {3}", DateTime.Now.TimeOfDay, _monitors._memoryUsed.ToString("N2"), _monitors._procTime.ToString("N2"), _monitors._pageFaults);
             }
