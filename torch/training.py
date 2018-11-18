0f2f619f789ad4cca376a77f8b4998013f839134
Pannozzo <pannozzo@mail.usf.edu>
2018-11-18 13:02:44 -0500

    Update training.py
    Training model throws out-of-bounds errors with Adam during loss calc, occasionally with SGD but not as many. Need to figure out where it's coming from.

--- torch/training.py
+++ torch/training.py
@@ -14,13 +14,14 @@ from my_classes import my_model
 
 device = torch.device("cpu")
   
-dataset = my_data.dataset
+dataset = my_data.training_dataset
 
-X = dataset.tensor_metrics		#Data: CPU/RAM
+X = dataset.tensor_features		#Data: CPU/RAM
 Y = dataset.tensor_results		#Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
 
 model = my_model.MyModel()
 optimizer = optim.SGD(model.parameters(), model.learning_rate) 
+# optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
 criterion = nn.BCELoss(reduction = 'elementwise_mean')
 
 for i in range(model.training_iterations):
@@ -36,21 +37,3 @@ for i in range(model.training_iterations):
     optimizer.step() #updates params of lin reg model 
 
 model.save_weights()
-
-
-######## DELETE LATER
-
-def is_thrashing(prediction):
-  if prediction > 0.50:
-    print("THRASHING: %.2f" % (prediction * 100) + "%")
-  else:
-    print("NOT THRASHING: %.2f" % (prediction * 100) + "%")
-
-  
-model.eval()
-
-test_tensor_data = torch.FloatTensor([3, 3])
-
-prediction = model(test_tensor_data).item()
-
-is_thrashing(prediction)
