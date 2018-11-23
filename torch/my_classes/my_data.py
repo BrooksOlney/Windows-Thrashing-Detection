dab9cfb26e4bc71bb8278e26075c2264a5bd3dd2
Pannozzo <pannozzo@mail.usf.edu>
2018-11-23 14:05:49 -0500

    error catching
    When the CSV is being written to by C# component, Python will throw a PermissionError "Errno 13" when attempting to read it at the exact same time. Rarely, the fille will not exist during these re-writes, and it will encounter a FileNotFoundError. 
    
    The lines where the CSV is first opened, CSV is reloaded, and when data is being pulled from it may all encounter these errors. These lines have been wrapped in an unending while loop until the exception does not occur. Worst case scenario, the detector just quietly doesn't work if the file actually doesn't exist (which is ideal). 
    
    There's probably a better way to do this.

--- torch/my_classes/my_data.py
+++ torch/my_classes/my_data.py
@@ -10,9 +10,10 @@ from torch.utils.data import Dataset
 
 
 class TrashingDataset(Dataset):
+  
+  #Read in data, split testdata between features and binary results
   def __init__(self, is_training, csv_file):
     
-    #Read in data, split testdata between features and binary results
     self.is_training = is_training
 
     if is_training:
@@ -23,20 +24,33 @@ class TrashingDataset(Dataset):
         self.tensor_features = torch.tensor(self.features.values, dtype = torch.float)
         self.tensor_results = torch.tensor(self.results.values, dtype = torch.float)
     else:
-        # print("Is closed? : ", csv_file.closed)
-        self.last_line = csv_file.readlines()[-1].strip().split(",")
+        self.read_last_line(csv_file)
         self.test_data = [float(i) for i in self.last_line]    
         self.tensor_features = torch.tensor(self.test_data)
 
-
+  #Gets called each polling interval to load newest test vector
   def reload_data(self):
     if self.is_training:
         with open('data/training_set.csv', 'r') as train_data_csv:
             self.__init__(True, train_data_csv)
     else:
-        with open('data/testing_set.csv', 'r') as test_data_csv:
-            self.__init__(False, test_data_csv)
+        while(1):
+            try:
+                with open('data/testing_set.csv', 'r') as test_data_csv:
+                    self.__init__(False, test_data_csv)
+                    break
+            except (PermissionError, FileNotFoundError):
+                pass # Keep trying until it works
+
+  def read_last_line(self, csv_file):
+    while(1):
+        try:
+            self.last_line = csv_file.readlines()[-1].strip().split(",")
+            break
+        except (PermissionError, FileNotFoundError):
+            pass # Keep trying until it works
             
+
 
 with open('data/training_set.csv', 'r') as train_data_csv:
     training_dataset = TrashingDataset(True, train_data_csv)
