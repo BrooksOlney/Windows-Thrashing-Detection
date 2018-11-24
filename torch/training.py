a9bb3e87624d66d7300c265b19193983bbdc1fea
Pannozzo <pannozzo@mail.usf.edu>
2018-11-23 22:39:00 -0500

    Fixed loss function
    Changed to BCEWithLogitsLoss loss function which allows me to account for weighted data. If you have significantly more "Wins" than "Losses" in your data (or vice versa) you have to tell your model that. This lets me do that with the results column. Summing the entire column gives me the number of wins, number of rows div wins = loss:win rate. 
    
    This needs to be incorporated into the model itself. Print statements need to be removed.

--- torch/training.py
+++ torch/training.py
@@ -17,23 +17,37 @@ device = torch.device("cpu")
 dataset = my_data.training_dataset
 
 X = dataset.tensor_features		#Data: CPU/RAM
-Y = dataset.tensor_results		#Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
+Y = dataset.tensor_results	
+	#Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
 
 model = my_model.MyModel()
-optimizer = optim.SGD(model.parameters(), model.learning_rate) 
-# optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-criterion = nn.BCELoss(reduction = 'elementwise_mean')
+# optimizer = optim.SGD(model.parameters(), model.learning_rate) 
+optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+
+#rename, move to model
+pweight = Y.shape[0] / torch.sum(Y)
+
+criterion = nn.BCEWithLogitsLoss(reduction = 'elementwise_mean', pos_weight = pweight )
+
+model.train()
+
+#delete later
+Y_next = model(X)
+loss = criterion(Y_next, Y)
+optimizer.zero_grad()
+loss.backward(retain_graph = True)
+optimizer.step() #updates params of lin reg model 
 
 for i in range(model.training_iterations):
-    model.train()
     Y_next = model(X)
-	
+
     loss = criterion(Y_next, Y)
+
     if i % 500 == 0:
       print(i, loss.item())
     
     optimizer.zero_grad()
-    loss.backward()
+    loss.backward(retain_graph = True)
     optimizer.step() #updates params of lin reg model 
 
 model.save_weights()
