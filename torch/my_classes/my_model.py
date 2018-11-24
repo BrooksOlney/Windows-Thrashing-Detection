84bb7cca0a6ac81134af94ec737689ce62e3d0cf
Pannozzo <pannozzo@mail.usf.edu>
2018-11-23 22:44:35 -0500

    minor edits
    Clean up, adjusting learn/training iter rate

--- torch/my_classes/my_model.py
+++ torch/my_classes/my_model.py
@@ -19,19 +19,15 @@ class MyModel(nn.Module):
     self.output_dimensions = 1
     self.hidden_layer = 2
     
-    self.learning_rate = 0.001
-    self.training_iterations = 25000
+    self.learning_rate = 0.01
+    self.training_iterations = 2500 #NAN errors over 3000 currently
     
     self.linear_layer_1 = nn.Linear(self.input_dimensions, self.hidden_layer)
     self.linear_layer_2 = nn.Linear(self.hidden_layer, self.output_dimensions)
     
   #Whenever __call__ is made, this is run
   def forward(self, x):
-
-    # layer1 = F.relu(self.linear_layer_1(x))
-    # layer2 = F.relu(self.linear_layer_2(layer1))
-
-    layer1 = self.sigmoid(self.linear_layer_1(x)) 
+    layer1 = self.sigmoid(self.linear_layer_1(x))
     layer2 = self.sigmoid(self.linear_layer_2(layer1))
     return layer2
   
