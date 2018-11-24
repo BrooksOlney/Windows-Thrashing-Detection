dbb10a0aff15441685725d2a39153daef85e4128
Pannozzo <pannozzo@mail.usf.edu>
2018-11-23 22:36:30 -0500

    Minor edits
    Polling interval needs to be in config file instead of magic number

--- torch/prediction.py
+++ torch/prediction.py
@@ -12,21 +12,16 @@ from torch.utils.data import Dataset
 from my_classes import my_data
 from my_classes import my_model
 
+MAX_COUNT = 30
+POLLING_INTERVAL = 0.125
+
 class ThrashingDetector:
 	def __init__(self):
 		self.counter = 0
 
 	def predict(self, prediction):
-		if prediction > 0.50:
-			self.counter += 1
-		else:
-			self.counter -= 1
-
-		if self.counter > 30:
-			print("ALERT: THRASHING")
-		elif self.counter < 0:
-			self.counter = 0
-
+		step = 1 if prediction > 0.50 else -1
+		self.counter = max(0, min(MAX_COUNT, self.counter + step))
 
 device = torch.device("cpu")
 
@@ -36,23 +31,13 @@ X = dataset.tensor_features
 
 
 model = my_model.MyModel()
-model.load_weights()
-
-# def is_thrashing(prediction):
-# 	if prediction > 0.50:
-# 		print("THRASHING: %.2f" % (prediction * 100) + "%")
-# 		return 1
-# 	else:
-# 		print("NOT THRASHING: %.2f" % (prediction * 100) + "%")
-# 		return -1
-
- 
+model.load_weights() 
 model.eval()
 
 detector = ThrashingDetector()
 
-while(detector.counter < 10):
-	time.sleep(1)
+while(detector.counter < MAX_COUNT):
+	time.sleep(POLLING_INTERVAL)
 
 	dataset.reload_data()
 	
@@ -61,7 +46,7 @@ while(detector.counter < 10):
 	prediction = model(test_tensor_data).item()
 	
 	detector.predict(prediction)
-	
 	print(detector.counter)
+	print(prediction * 100, "%")
 
 print("THRASHING!!!")
