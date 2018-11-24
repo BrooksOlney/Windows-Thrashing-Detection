30e3f11bc4003b3476ada71f59a1eca9bc9a7259
Pannozzo <pannozzo@mail.usf.edu>
2018-11-23 23:35:05 -0500

    Tidying

--- torch/training.py
+++ torch/training.py
@@ -16,38 +16,29 @@ device = torch.device("cpu")
   
 dataset = my_data.training_dataset
 
-X = dataset.tensor_features		#Data: CPU/RAM
-Y = dataset.tensor_results	
-	#Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
-
-model = my_model.MyModel()
-# optimizer = optim.SGD(model.parameters(), model.learning_rate) 
-optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+X = dataset.tensor_features	  #Data: CPU/RAM
+Y = dataset.tensor_results	  #Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
 
 #rename, move to model
 pweight = Y.shape[0] / torch.sum(Y)
 
+model = my_model.MyModel() 
+optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
 criterion = nn.BCEWithLogitsLoss(reduction = 'elementwise_mean', pos_weight = pweight )
 
 model.train()
-
-#delete later
-Y_next = model(X)
-loss = criterion(Y_next, Y)
-optimizer.zero_grad()
-loss.backward(retain_graph = True)
-optimizer.step() #updates params of lin reg model 
 
 for i in range(model.training_iterations):
     Y_next = model(X)
 
     loss = criterion(Y_next, Y)
 
+    #Debug info, delete later
     if i % 500 == 0:
       print(i, loss.item())
     
     optimizer.zero_grad()
-    loss.backward(retain_graph = True)
+    loss.backward(retain_graph = True) #Do I need retain_graph? Look into
     optimizer.step() #updates params of lin reg model 
 
 model.save_weights()
