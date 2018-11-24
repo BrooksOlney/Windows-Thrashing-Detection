5fd6516b6f90c2cf6e1a037465b872c9af62d3b8
Pannozzo <pannozzo@mail.usf.edu>
2018-11-24 16:25:59 -0500

    Update training.py

--- torch/training.py
+++ torch/training.py
@@ -8,23 +8,21 @@ from torch import nn
 from torch import optim
 from torch.utils.data import Dataset
 
-from my_classes import my_data
+from my_classes import training_data
 from my_classes import my_model
 
 
 device = torch.device("cpu")
   
-dataset = my_data.training_dataset
+dataset = training_data.training_dataset
 
 X = dataset.tensor_features	  #Data: CPU/RAM
 Y = dataset.tensor_results	  #Binary 1/0 was this CPU/RAM case thrashing, for BCELoss 
 
-#rename, move to model
-pweight = Y.shape[0] / torch.sum(Y)
 
 model = my_model.MyModel() 
 optimizer = optim.Adam(model.parameters(), model.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-criterion = nn.BCEWithLogitsLoss(reduction = 'elementwise_mean', pos_weight = pweight )
+criterion = nn.BCEWithLogitsLoss(reduction = 'elementwise_mean', pos_weight = dataset.pos_weight)
 
 model.train()
 
